c3ddce537d7ff6f51f30d524ba3c853054147249
Kryštof Matějka <kx.matejka@gmail.com>
2020-02-02 15:32:05 +0100

    implement a naive way how to fetch data from db

---
+++ apps/graphql-apollo/README.md
@@ -0,0 +1,3 @@
+# Almighty root resolver
+
+This is inspired by https://blog.smartive.ch/building-enterprise-grade-apis-with-graphql-mysql-and-node-js-a411a5a612f1
--- apps/graphql-apollo/src/index.ts
+++ apps/graphql-apollo/src/index.ts
@@ -1,13 +1,33 @@
 import {ApolloServer, gql} from 'apollo-server'
+import Knex from 'knex'
+
+const db = Knex({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'blog'
+  }
+})
 
 const typeDefs = gql`
   type Post {
     id: ID
     title: String
+    user: User
+  }
+
+  type User {
+    id: ID
+    name: String
+    posts: [Post]
   }
 
   type Query {
+    post(id: String!): Post
     posts: [Post]
+    me: User
   }
 
   schema {
@@ -15,25 +35,38 @@ const typeDefs = gql`
   }
 `
 
-interface Post {
-  id: string,
-  title: string
-}
-
-const posts: Post[] = [
-  {
-    id: 'abcd',
-    title: 'Koule'
-  },
-  {
-    id: 'aece',
-    title: 'ptak'
-  }
-]
-
 const resolvers = {
   Query: {
-    posts: () => posts
+    post: async (obj, args) => {
+      const post = await db.from('posts').where({id: args.id}).first()
+      const user = await db.from('users').where({id: post.user_id}).first()
+
+      return {
+        ...post,
+        user
+      }
+    },
+    posts: async (obj, args, context, info) => {
+      const posts = await db.from('posts')
+
+      return posts.map(async post => {
+        const user = await db.from('users').where({id: post.user_id}).first()
+
+        return {
+          ...post,
+          user
+        }
+      })
+    },
+    me: async () => {
+      const user = await db.from('users').where({id: 1}).first()
+      const posts = await db.from('posts').where({user_id: 1})
+
+      return {
+        ...user,
+        posts
+      }
+    }
   }
 }
 
